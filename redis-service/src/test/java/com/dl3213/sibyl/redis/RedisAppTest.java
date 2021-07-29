310e67a11e75681151fbc38896f631f044ad1b69
dl3213 <dl3213@192.168.0.101>
2021-07-29 23:22:11 +0800

    date => 2021.7.29

---
+++ redis-service/src/test/java/com/dl3213/sibyl/redis/RedisAppTest.java
@@ -0,0 +1,33 @@
+package com.dl3213.sibyl.redis;
+
+import com.dl3213.sibyl.redis.pojo.Player;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+
+/**
+ * @Classname com.dl3213.sibyl.redis.RedisAppTest
+ * @Description TODO
+ * @Date 2021/7/29 22:38
+ * @Created by dyingleaf3213
+ */
+@SpringBootTest(classes = RedisApp.class)
+public class RedisAppTest {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @SneakyThrows
+    @Test
+    public void redisTest(){
+        System.err.println("****");
+        Player player = new Player(1L, "dl3213", "player1");
+        String json = new ObjectMapper().writeValueAsString(player);
+        redisTemplate.opsForValue().set("player1",json);
+        System.err.println(redisTemplate.opsForValue().get("player1"));
+    }
+}
