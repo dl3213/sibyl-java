5ef073af452f268e8ce3e3fc6f0a81873e38e112
dl3213 <dl3213@192.168.0.101>
2022-03-05 23:23:45 +0800

    update v0.2.1

--- application/src/main/java/com/sibyl/Application.java
+++ application/src/main/java/com/sibyl/Application.java
@@ -2,13 +2,13 @@ package com.sibyl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableAsync
-//@EnableRetry
+@EnableRetry
 public class Application {
 
     public static void main(String[] args) {
